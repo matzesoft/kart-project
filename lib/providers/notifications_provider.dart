38ec6be64bc6e6a958feb916c5c213e974b86088
matzesoft <48252507+matzesoft@users.noreply.github.com>
2021-02-19 22:53:43 +0100

    Bigger touch region for notifications

--- lib/providers/notifications_provider.dart
+++ lib/providers/notifications_provider.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:kart_project/design/overlay_notification.dart';
 import 'package:overlay_support/overlay_support.dart';
 
 class NotificationsProvider {
@@ -88,60 +89,3 @@ class NotificationsProvider {
   }
 }
 
-/// Overlay normally presented at the bottom of the screen. Takes a [icon] and
-/// the [message] as the confirmation message.
-class OvlerayNotification extends StatelessWidget {
-  final double _widthFactor = 0.5;
-  final IconData icon;
-  final String message;
-  final Function onDismissed;
-  final int maxLines;
-
-  OvlerayNotification({
-    this.icon: EvaIcons.infoOutline,
-    this.message: "",
-    this.onDismissed,
-    this.maxLines: 1,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Dismissible(
-      key: Key("key"),
-      direction: DismissDirection.down,
-      onDismissed: (_) {
-        onDismissed();
-      },
-      child: FractionallySizedBox(
-        widthFactor: _widthFactor,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Card(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Icon(icon),
-                  ),
-                  Flexible(
-                    child: Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text(
-                        message,
-                        maxLines: maxLines,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
