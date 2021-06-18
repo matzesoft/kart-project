f7ca6f1decf84a14e2805651d4f5bc785d1390fc
matzesoft <48252507+matzesoft@users.noreply.github.com>
2021-06-18 18:35:27 +0200

    Added debug mode arg

--- run/run.sh
+++ run/run.sh
@@ -3,9 +3,17 @@
 # This script runs the kart_project on the RaspberryPi.
 # Should be located under '/home/pi'.
 
+project_name="kart_project_release"
+project_type="--release"
+
+if [ "$1" = "-d" ]; then
+    project_name="kart_project_debug"
+    project_type=""
+fi
+
 counter=0 # Counter how often flutter-pi failed to run.
 while [ $counter -lt 10 ]; do
-    sudo /home/pi/flutter-pi/build/flutter-pi -r 180 /home/pi/projects/kart_project | tee /home/pi/logs/log.txt-$(date +"%Y-%m-%d-%T")
+    sudo /home/pi/flutter-pi/build/flutter-pi -r 180 $project_type /home/pi/projects/$project_name | tee /home/pi/logs/log.txt-$(date +"%Y-%m-%d-%T")
     echo "KartProject will be restarted in 3 seconds..."
     sleep 3s
     counter=$(expr $counter + 1)
